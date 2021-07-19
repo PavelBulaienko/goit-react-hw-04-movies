7d72ba4153cec1d70e54dd889c5447ad4f3ad71f
PavelBulaienko <57744770+PavelBulaienko@users.noreply.github.com>
2021-07-19 18:12:56 +0300

    change

--- src/services/moviesDetailPageAPI.js
+++ src/services/moviesDetailPageAPI.js
@@ -1,24 +1,26 @@
+import axios from 'axios'
+
 const moviesDetailPageAPI = {
   api_key: '745843703988462346ba909afe11c7ba',
 
   async fullInfoMovie(movieId) {
-    const responce = await fetch(
+    const responce = await axios(
       `https:api.themoviedb.org/3/movie/${movieId}&?api_key=${this.api_key}`
-    ).then((responce) => responce.json())
+    ).then((responce) => responce.data)
 
     return responce
   },
   async castInfoMovie(movieId) {
-    const responce = await fetch(
+    const responce = await axios(
       `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${this.api_key}`
-    ).then((responce) => responce.json())
+    ).then((responce) => responce.data)
 
     return responce
   },
   async reviewsInfoMovie(movieId) {
-    const responce = await fetch(
+    const responce = await axios(
       `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${this.api_key}`
-    ).then((responce) => responce.json())
+    ).then((responce) => responce.data)
 
     return responce
   },
